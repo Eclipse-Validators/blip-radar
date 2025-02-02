a1202a7b55614bf2fe3bfa178b09d1006f864fef
rob shaw <rob@statik.dev>
2025-02-02 18:29:00 -0500

    test: comment out live test

--- tests/blip-radar.ts
+++ tests/blip-radar.ts
@@ -24,62 +24,62 @@ export function loadAuthorityWallet(): anchor.web3.Keypair {
 describe("blip-radar", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const conn = new anchor.web3.Connection(
-    "https://staging-rpc.dev2.eclipsenetwork.xyz",
-    "finalized"
-  );
-  const payer = new anchor.web3.Keypair();
-  anchor.setProvider(new anchor.AnchorProvider(conn, new anchor.Wallet(payer)));
+  // const conn = new anchor.web3.Connection(
+  //   "https://staging-rpc.dev2.eclipsenetwork.xyz",
+  //   "finalized"
+  // );
+  // const payer = new anchor.web3.Keypair();
+  // anchor.setProvider(new anchor.AnchorProvider(conn, new anchor.Wallet(payer)));
 
-  const program = anchor.workspace.BlipRadar as Program<BlipRadar>;
+  // const program = anchor.workspace.BlipRadar as Program<BlipRadar>;
 
-  it("Is initialized!", async () => {
-    const asset = anchor.web3.Keypair.generate();
+  // it("Is initialized!", async () => {
+  //   const asset = anchor.web3.Keypair.generate();
 
-    const feeAccount = new anchor.web3.PublicKey(
-      "6juCmFHoPnJTzhjJfcjFhCXeptCE89vp9dHP91EUaxR8"
-    );
+  //   const feeAccount = new anchor.web3.PublicKey(
+  //     "6juCmFHoPnJTzhjJfcjFhCXeptCE89vp9dHP91EUaxR8"
+  //   );
 
-    const receiverAccount = anchor.web3.Keypair.generate();
+  //   const receiverAccount = anchor.web3.Keypair.generate();
 
-    const authority = loadAuthorityWallet();
-    const receiverSig = await conn.requestAirdrop(
-      receiverAccount.publicKey,
-      1_000_000 * 1
-    );
-    const payerSig = await conn.requestAirdrop(
-      payer.publicKey,
-      1_000_000_000 * 1
-    );
+  //   const authority = loadAuthorityWallet();
+  //   const receiverSig = await conn.requestAirdrop(
+  //     receiverAccount.publicKey,
+  //     1_000_000 * 1
+  //   );
+  //   const payerSig = await conn.requestAirdrop(
+  //     payer.publicKey,
+  //     1_000_000_000 * 1
+  //   );
 
-    console.log("payer", payer.publicKey);
-    console.log("reciever", receiverAccount.publicKey);
+  //   console.log("payer", payer.publicKey);
+  //   console.log("reciever", receiverAccount.publicKey);
 
-    await conn.confirmTransaction(receiverSig);
-    await conn.confirmTransaction(payerSig);
+  //   await conn.confirmTransaction(receiverSig);
+  //   await conn.confirmTransaction(payerSig);
 
-    const tx = await program.methods
-      .sendBlip(
-        "https://arweave.net/8FDIo_3e5EJBx_1Nq9tdeZ1XZHN2_3GDomncAYKw1jM"
-      )
-      .accountsPartial({
-        asset: asset.publicKey,
-        payer: payer.publicKey,
-        receiver: receiverAccount.publicKey,
-        feeDestination: feeAccount,
-        mplCoreProgram: new anchor.web3.PublicKey(
-          "CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d"
-        ),
-        collection: new anchor.web3.PublicKey(
-          "HZp57kjtHqxptkn5KL4nJsZV5LYvGWb2oteGtB3t93wy"
-        ),
-        collectionAuthority: authority.publicKey,
-        systemProgram: new anchor.web3.PublicKey(
-          "11111111111111111111111111111111"
-        ),
-      })
-      .signers([asset, payer, authority])
-      .rpc();
-    console.log("Your transaction signature", tx);
-  });
+  //   const tx = await program.methods
+  //     .sendBlip(
+  //       "https://arweave.net/8FDIo_3e5EJBx_1Nq9tdeZ1XZHN2_3GDomncAYKw1jM"
+  //     )
+  //     .accountsPartial({
+  //       asset: asset.publicKey,
+  //       payer: payer.publicKey,
+  //       receiver: receiverAccount.publicKey,
+  //       feeDestination: feeAccount,
+  //       mplCoreProgram: new anchor.web3.PublicKey(
+  //         "CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d"
+  //       ),
+  //       collection: new anchor.web3.PublicKey(
+  //         "HZp57kjtHqxptkn5KL4nJsZV5LYvGWb2oteGtB3t93wy"
+  //       ),
+  //       collectionAuthority: authority.publicKey,
+  //       systemProgram: new anchor.web3.PublicKey(
+  //         "11111111111111111111111111111111"
+  //       ),
+  //     })
+  //     .signers([asset, payer, authority])
+  //     .rpc();
+  //   console.log("Your transaction signature", tx);
+  // });
 });
